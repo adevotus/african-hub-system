aaf768cc78a48b7017b3a4a43d3757d18875956b
adevotus <adickson@credable.io>
2025-03-04 18:30:22 +0300

    update

--- database/migrations/2025_03_02_200247_create_password_resets_table.php
+++ database/migrations/2025_03_02_200247_create_password_resets_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-
-        });
+//        Schema::create('password_resets', function (Blueprint $table) {
+//            $table->string('email')->index();
+//            $table->string('token');
+//            $table->timestamp('created_at')->nullable();
+//
+//        });
     }
 
     /**
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+//        Schema::dropIfExists('password_resets');
     }
 };
